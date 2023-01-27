090d322e1579b0e0d3467f357859e5e1bf6b91c3
SatwikPV <satwikpv@gmail.com>
2023-01-27 14:39:14 +0530

    delete again

--- LAB7/numOfVowels.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/sh
-echo "Enter the string"
-read s
-count=$(echo $s | grep -o -i "[aeiou]" | wc -l )
-echo "Count of vowels: $count"
--- LAB8/patternUsingShift.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/sh/
-
-#pattern using shift command
